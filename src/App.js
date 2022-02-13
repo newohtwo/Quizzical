e992067a38d9ec4e03f2c0f694bb85be7b72ea24
Newohtwo <stasvish1317@gmail.com>
2022-02-13 23:14:12 +0200

    initial page introduction created , api fetch test done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import './App.css';
+import IntroductionPage from './components/IntroductionPage';
+import react from 'react';
+import axios from 'axios';
 
 function App() {
-  return <div className="App"></div>;
+  const [triviaQuastions, setTriviaQuastions] = react.useState();
+  const [gameChoice, setGameChoice] = react.useState(false);
+
+  function createApiCall(userPrefrences) {
+    console.log(userPrefrences);
+    const url = `https://opentdb.com/api.php?amount=${userPrefrences.quastionNum}&category=${userPrefrences.category}&difficulty=${userPrefrences.difficulty}&type=multiple`;
+    fetchTrivia(url);
+  }
+  //TODO FIND A WAY TO USE THE GOTTEN DATA INTO THE NEXT PAGE OF THE APP
+  //SHOW THE DATA IN A QUASTION AND ANSWER WAY ,
+  //SHOW CORRECT AND INCORRECT ANSWERS
+  async function fetchTrivia(url) {
+    const response = await axios.get(url);
+    setTriviaQuastions(response.data.results);
+  }
+
+  function test() {
+    console.log(triviaQuastions);
+  }
+
+  return (
+    <div className="App container page rounded  mt-4">
+      {!gameChoice && <IntroductionPage createApiCall={createApiCall} />}
+      <button onClick={test}>asdf</button>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/IntroductionPage.js
@@ -0,0 +1,95 @@
+import react from 'react';
+import data from './TriviaCategoriesData';
+//most likely i can generalise the select options
+
+export default function IntroductionPage(props) {
+  // const [quastionsNum, setQuastionsNum] = react.useState(5);
+  // const [difficulty, setDifficulty] = react.useState('Easy');
+  // const [category, setCategory] = react.useState(data[0].id);
+
+  const [form, setForm] = react.useState({
+    quastionNum: 5,
+    difficulty: 'easy',
+    category: '9',
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setForm((oldForm) => {
+      return { ...oldForm, [name]: value };
+    });
+  }
+
+  //initalize categories from data
+  function initCategories() {
+    return data.map((category) => {
+      return (
+        <option key={category.id} value={category.id}>
+          {category.name}
+        </option>
+      );
+    });
+  }
+
+  return (
+    <div className="container ">
+      <div className="container text-center">
+        <h1 className="pt-4">Quizzical</h1>
+        <p>Trivia game about anything and everything!</p>
+      </div>
+
+      <div className=" container">
+        <h3>Game Settings:</h3>
+        <div>
+          <label className="game-setting-select">
+            Number Of Quastions:
+            <select
+              className="form-select"
+              onChange={handleChange}
+              name="quastionNum"
+            >
+              <option value="5">5</option>
+              <option value="10">10</option>
+              <option value="20">20</option>
+              <option value="50">50</option>
+            </select>
+          </label>
+        </div>
+
+        <div>
+          <label className="game-setting-select">
+            Difficulty:
+            <select
+              className="form-select"
+              onChange={handleChange}
+              name="difficulty"
+            >
+              <option value="easy">Easy</option>
+              <option value="medium">Medium</option>
+              <option value="hard">Hard</option>
+            </select>
+          </label>
+        </div>
+
+        <div>
+          <label className="game-setting-select">
+            Select Category:
+            <select
+              className="form-select"
+              onChange={handleChange}
+              name="category"
+            >
+              {initCategories()}
+            </select>
+          </label>
+        </div>
+
+        <input
+          type="submit"
+          value="Start Game!"
+          onClick={() => props.createApiCall(form)}
+        />
+      </div>
+    </div>
+  );
+}
